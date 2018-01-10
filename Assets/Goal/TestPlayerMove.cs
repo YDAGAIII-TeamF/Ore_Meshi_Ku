d2bc8c44a141df3637fc559b3246fe80fe71759f
yuutaro-O <yuutarousoft@gmail.com>
2018-01-10 11:18:12 +0900

    （大塚）：Goalの作成、Goal時の処理の仮記述

---
+++ Assets/Goal/TestPlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MeshiScript
+{
+    public class TestPlayerMove : MonoBehaviour
+    {
+        Transform thisTransform;
+        // Use this for initialization
+        void Start()
+        {
+            thisTransform = gameObject.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            thisTransform.position = new Vector3(thisTransform.position.x + (Input.GetAxis("Horizontal") * Time.deltaTime), thisTransform.position.y, thisTransform.position.z + Input.GetAxis("Vertical") * Time.deltaTime);
+        }
+    }
+}
