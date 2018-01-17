be07fdc86b0c18d731d16fde444b486c4ce0f9af
yuutaro-O <yuutarousoft@gmail.com>
2018-01-17 10:38:58 +0900

    （大塚）：ゴール地点管理クラス・オブジェクトの作成

---
+++ Assets/GoalManager/GoalManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshiScript
+{
+    public class GoalManager : MonoBehaviour
+    {
+        MeshiScript.GoalPointManager ref_GoalPointManager;
+        Transform thisTransform;
+        [SerializeField]
+        GameObject Prefab_Goal;
+        private void Awake()
+        {
+            ref_GoalPointManager = transform.GetChild(0).gameObject.GetComponent<MeshiScript.GoalPointManager>();
+            thisTransform = gameObject.transform;
+        }
+        // Use this for initialization
+        void Start()
+        {
+            int GoalPointNum = ref_GoalPointManager.GoalPoints.Count;
+            int CorrectGoalPointNumber = Random.Range(0, GoalPointNum);
+            for (int i = 0; i < GoalPointNum; i++) {
+                if (i == CorrectGoalPointNumber)
+                {
+                    Instantiate<GameObject>(Prefab_Goal, ref_GoalPointManager.GoalPoints[i].transform.position, Quaternion.Euler(90, 0, 0), thisTransform);
+                }
+            }
+        }
+
+        
+    }
+}
