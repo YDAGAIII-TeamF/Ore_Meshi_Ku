f29effe6a51f6f9b4f44cdae268eb11bc3bc645b
yuutaro-O <yuutarousoft@gmail.com>
2018-01-30 09:34:56 +0900

    （大塚）：CarManagerの作成
    車の生成と破棄を管理するCarManager(通称：車ネージャー)の作成。
    ・CarManager内のSpawnPointに生成地点を、DespawnPointに車の消去地点を空のゲームオブジェクトとして格納すれば機能するようにしている。

---
+++ Assets/CarManager/CarManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshiScript
+{
+    public class CarManager : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject Prefab_Car;
+        [SerializeField]
+        List<Transform> CarSpawnPoint;
+        [SerializeField]
+        Transform CarSpawnParentTransform;
+
+        [SerializeField]
+        float minTime;
+        [SerializeField]
+        float maxTime;
+        float nextTime;
+        float nowTime;
+
+        [SerializeField]
+        List<Transform> CarDespawnPoint;
+        [SerializeField]
+        GameObject Prefab_CarDespawnPoint;
+        [SerializeField]
+        Transform CarDespawnParentTransform;
+
+        // Use this for initialization
+        private void Awake()
+        {
+            //車生成地点関連処理
+            CarSpawnPoint = new List<Transform>();
+
+            for(int i = 0;i < CarSpawnParentTransform.childCount; i++)
+            {
+                CarSpawnPoint.Add(CarSpawnParentTransform.GetChild(i).transform);
+            }
+
+            if(minTime < 0.0f)
+            {
+                minTime = 0.0f;
+            }
+
+            if(minTime > maxTime)
+            {
+                maxTime = minTime;
+            }
+            //車破棄地点関連処理
+            CarDespawnPoint = new List<Transform>();
+
+            for(int i = 0; i < CarDespawnParentTransform.childCount; i++)
+            {
+                CarDespawnPoint.Add(CarDespawnParentTransform.GetChild(i));
+                InstantiateDespawnPoint(i);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            nowTime += Time.deltaTime;
+            if (nowTime >= nextTime)
+            {
+                SpawnCar(Random.Range(0, CarDespawnPoint.Count));
+                nowTime = 0.0f;
+                nextTime = Random.Range(minTime, maxTime);
+            }
+        }
+
+        void SpawnCar(int spawnIndex)
+        {
+            Instantiate<GameObject>(Prefab_Car,CarSpawnPoint[spawnIndex].position,Quaternion.Euler(90,0,0));
+        }
+
+        void InstantiateDespawnPoint(int spawnIndex)
+        {
+            Instantiate<GameObject>(Prefab_CarDespawnPoint, CarDespawnPoint[spawnIndex].position, Quaternion.Euler(0, 0, 0));
+        }
+    }
+}
