5fbbe7f5e147568c47f246a0992e7fc947a32e14
yuutaro-O <yuutarousoft@gmail.com>
2018-01-31 10:13:47 +0900

    （大塚）：Playerの移動軸の修正
    上下移動キーを押した際にz軸ではなくy軸に移動していた問題の修正

--- Assets/Player/Plyaer.cs
+++ Assets/Player/Plyaer.cs
@@ -8,21 +8,21 @@ namespace MeshiScript
     {
         public float Plyaer_Speed;
         public static float Satiety = 1;
-
+        Rigidbody rigidBody;
         void Start()
         {
-
+            rigidBody = GetComponent<Rigidbody>();
         }
 
         void Update()
         {
             float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
-            float z = 0;
+            float y = 0;
+            float z = Input.GetAxis("Vertical");
 
             Vector3 direction = new Vector3(x, y, z).normalized;
 
-            GetComponent<Rigidbody2D>().velocity = direction * Plyaer_Speed * Satiety;
+            rigidBody.velocity = direction * Plyaer_Speed * Satiety;
         }
     }
 }
